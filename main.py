43b914c4db298575f6f0b57de431a055cc7e4a87
jiangzhe <312985123@qq.com>
2021-05-18 19:42:25 +0900

    “1234”

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-for i in range(1,10):
-    for k in range(1,10-i):
-        print(end="        ")
-    for j in range(1,i+1):
-        n = i * j
-        print(str(j)+"*"+str(i)+"="+str(n)+"\t",end="")
-
-    print()
+class Solution:
+    def twoSum(self,nums,target):
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target:
+                    return[i,j]
+                print
+        return []
+# nums = [2, 7, 11, 15]
+nums = [3, 2, 3, 4, 11, 7, 4]
+target = 9
+a = Solution()
+out = a.twoSum(nums,target)
+print('结果：',out)
 
 
