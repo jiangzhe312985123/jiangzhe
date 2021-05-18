08f4003afcfccb5b87dbfb948adb0df759c28993
jiangzhe <312985123@qq.com>
2021-05-18 11:02:26 +0900

    “1234”

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for row in range(1, 10):
-    for col in range(1, row+1):
-        print('{}*{}={}'.format(col, row, col * row), end='\t')
-    print()
+def solution(nums,target):
+	#如果列表长度小于2，则直接结束
+    if len(nums) < 2:
+        return
+        #两次循环列表，分别对列表中的所有可能的数字进行相加
+        for i in range(0, len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
