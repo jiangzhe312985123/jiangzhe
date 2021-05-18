b950314de9ec2722dd9dc1b0737b62ff95631ed0
jiangzhe <312985123@qq.com>
2021-05-18 16:35:11 +0900

    “1234”

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-for i in range(1, 10):
-    for j in range(1, i+1):
-        n = j * i
-        # 1*1  1*1,2   1*1,2,3
-        # print('{}*{}={}'.format(i,j,i*j), end='\t')
-        # print("%d * %d = %d" %i,j,i*j)
-        # print("i*j",end=/t)
-        # print(str(i)+"*"+str(j)+'='+str(n)+'\t', end="")
-        print(str(j), '*', str(i), '=', str(n), '\t', end="")
-    print()
-
-
 for i in range(1,10):
-    for j in range(1,i+1):
-        n = i * j
-        print(str(i)+"*"+str(j)+"="+str(n)+"\t",end='')
+    for k in range(1,10-i):
+        print(end="")
+    for j in range(1,10-i):
+        n = j * i
+        print(str(j)+"*"+str(i)+"="+str(n)+"\t",end="")
     print()
 
 
