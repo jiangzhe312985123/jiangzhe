e051bf0fe6ed0cdff20ee398f68ed0bd8b6a5416
jiangzhe <312985123@qq.com>
2021-05-20 16:17:28 +0900

    “1234”

--- main.py
+++ main.py
@@ -1,48 +1,40 @@
-# class Solution:
-#     def twoSum(self,nums,target):
-#         for i in range(len(nums)-1):
-#             for j in range(i+1,len(nums)):
-#                 if nums[i]+nums[j] == target:
-#                     return[i,j]
-# nums = [2, 7, 11, 15]
-# nums = [3, 2, 3, 4, 11, 7, 4]
-# target = 9
-# a = Solution()
-# out = a.twoSum(nums,target)
-# print('结果：',out)
-class Solution:
-    def towSum(self,nums,target):
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return[i,j]
-nums = [2,3,6,7,8,9]
-target = 9
-a = Solution()
-out = a.towSum(nums,target)
-print("数组:",nums,"目标:",target,"结果:",out)
+#两个变量值发生变换有两种方法，一种是python自带的简单的那种
+#冒泡法就是两个数字交换，两两交换
+# a=10
+# b=15
+# c=a 10
+# a=b  15
+# b=c  10
+# print(a)
+# print(b)
 
 
-class Solution:
-    def twoSum(self,nums,target):
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return[i,j]
-nums=[1,0]
-target=1
-a=Solution()
-out=a.twoSum(nums,target)
-print(out)
+# a,b =b,a
+# print(a)
+# print(b)
 
-class Solution:
-    def towSum(self,nums,target):
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return[i,j]
-nums=[1,2,3,6,8,9]
-target=9
-a=Solution()
-out=a.towSum(nums,target)
-print(out )
+# import random
+# test = []
+# for i in range(100):
+#     test.append(random.randint(0,1000))
+# def bubble(test):
+#     for k in range(0,len(test)-1):
+#         for j in range(0,len(test)-1-k):
+#             if test[j] > test[j+1]:
+#                 a=test[j]
+#                 test[j]= test[j+1]
+#                 test[j+1]=a
+#     return(test)
+# bubble(test)
+# print(test)
+import random
+test = []
+for i in range(100):
+    test.append(random.randint(0,1000))
+def order(test):
+    for k in range(0,len(test)-1):
+        for j in range(0,len(test)-1):
+            if test[j] > test[j+1]:
+                test[j],test[j+1] = test[j+1],test[j]
+    print(test)
+order(test)
