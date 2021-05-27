eac0298be06c5d7b91d36e52e333afbff3263035
jiangzhe <312985123@qq.com>
2021-05-27 16:26:20 +0900

    “1234”

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Solution:
         for i, j in enumerate(nums):#i是下标index，j是里面数字item
             if target - nums[i] in dict:#从第一个开始找，如果在表中找到了，就输出结果，如果没有在哈希表中找到，就存入哈希表中、
                 return i, dict[target - nums[i]]#dict[target - nums[i]]是下标
-            dict[j] = i #代表着哈希表（新建的字典）输出结果是对应的下标值，也就是2=0,3=1,6=2
+            dict[j] = i #代表着哈希表（新建的字典）输出结果是对应的下标值，也就是2=0,3=1,
 nums = [2,3,6,8,9]
 target = 9
 a = Solution()
