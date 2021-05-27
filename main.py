b68e5c848190c4f880996e1c67a0a61689337bb7
jiangzhe <312985123@qq.com>
2021-05-27 16:18:26 +0900

    “1234”

--- main.py
+++ main.py
@@ -1,57 +1,12 @@
-#两个变量值发生变换有两种方法，一种是python自带的简单的那种
-#冒泡法就是两个数字交换，两两交换
-# a=10
-# b=15
-# c=a 10
-# a=b  15
-# b=c  10
-# print(a)
-# print(b)
-
-
-# a,b =b,a
-# print(a)
-# print(b)
-
-# import random
-# test = []
-# for i in range(100):
-#     test.append(random.randint(0,1000))
-# def bubble(test):
-#     for k in range(0,len(test)-1):
-#         for j in range(0,len(test)-1-k):
-#             if test[j] > test[j+1]:
-#                 a=test[j]
-#                 test[j]= test[j+1]
-#                 test[j+1]=a
-#     return(test)
-# bubble(test)
-# print(test)
-
-# import random
-# test = []
-# for i in range(100):
-#     test.append(random.randint(0,1000))
-# def order(test):
-#     for k in range(0,len(test)-1):
-#         for j in range(0,len(test)-1):
-#             if test[j] > test[j+1]:
-#                 test[j],test[j+1] = test[j+1],test[j]
-#     print(test)
-# order(test)#
-
-import random
-test = []
-for i in range(100):
-    test.append(random.randint(0,1000))
-def order(test):
-    for k in range(len(test)-1):    #-1是因为最后一次不用比了，已经出结果了,设定一个变量为False，如果元素之间交换了位置，将变量重新赋值为True,最后再判断，在一次循环结束后，变量如果还是为False，则brak退出循环，结束排序。
-        flag = False
-        for j in range(len(test)-1-k):
-            if test[j] > test[j+1]:
-                test[j],test[j+1]=test[j+1],test[j]
-                flag = True
-        if not flag:
-            break
-    print(test)
-order(test)
+class Solution:
+    def towSum(self,nums,target):
+        dict = {}
+        for i, j in enumerate(nums):#i是下标index，j是里面数字item
+            if target - nums[i] in dict:#从第一个开始找，如果在表中找到了，就输出结果，如果没有在哈希表中找到，就存入哈希表中、
+                return i, dict[target - nums[i]]#dict[target - nums[i]]是下标
+            dict[j] = i #代表着哈希表（新建的字典）输出结果是对应的下标值，也就是2=0,3=1,6=2
+nums = [2,3,6,8,9]
+target = 9
+a = Solution()
+out = a.towSum(nums,target)
+print("数组:",nums,"目标:",target,"结果:",out)
